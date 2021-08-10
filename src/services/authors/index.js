84e0aad9e9ca10ef6a8c314036c19fbf6d0b397a
Radensuaso <andrels9283@gmail.com>
2021-08-10 14:29:47 +0100

    Basic responses set

--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -4,7 +4,23 @@ import express from "express" // We need to import express to use it's functiona
 const authorsRouter = express.Router() // Here we use Router express functionality to provide Routing to the server
 
 authorsRouter.post("/", (req, res) => {
-  res.status(201).send(req.body)
+  res.status(201).send(req.method)
+})
+
+authorsRouter.get("/", (req, res) => {
+  res.send(req.method)
+})
+
+authorsRouter.get("/:_id", (req, res) => {
+  res.send(req.method + " single")
+})
+
+authorsRouter.put("/:_id", (req, res) => {
+  res.send(req.method)
+})
+
+authorsRouter.delete("/:_id", (req, res) => {
+  res.status(204).send(req.method)
 })
 
 export default authorsRouter // Not to forget to export to be able to use it in the server

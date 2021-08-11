771eb405b888cae5cfdbf2dfa7e2809a0e04c6e3
Radensuaso <andrels9283@gmail.com>
2021-08-11 15:19:29 +0100

    The get endpoints are handled, the post in progress

--- src/server.js
+++ src/server.js
@@ -2,18 +2,22 @@ import express from "express" // import express from express
 import cors from "cors" // will enable the frontend to communicate with the backend
 import listEndpoints from "express-list-endpoints" // will show us the detailed endpoints
 import authorsRouter from "./services/authors/index.js"
+import blogPostsRouter from "./services/blogPosts/index.js"
 
 const server = express() //our server function initialized with express()
 const port = 3001 // this will be the port on with the server will run
 
+//=========== GLOBAL MIDDLEWARES ======================
 server.use(cors())
 server.use(express.json()) // this will enable reading of the bodies of requests, THIS HAS TO BE BEFORE server.use("/authors", authorsRouter)
 
-// ========== ROUTES =============
+// ========== ROUTES =======================
 
-server.use("/authors", authorsRouter) // this will provide the endpoints of authors with a common name to POST, GET, PUT and DELETE
-
+server.use("/authors", authorsRouter)
+server.use("/blogposts", blogPostsRouter) // this will provide the endpoints of authors with a common name to POST, GET, PUT and DELETE
 console.table(listEndpoints(server)) // will show us the detailed endpoints in a table
+
+// ============== ERROR HANDLING ==============
 
 server.listen(port, () =>
   console.log(`Server is listening to the port ${port}.`)
--- src/services/blogPosts/index.js
+++ src/services/blogPosts/index.js
@@ -1,10 +1,10 @@
 import express from "express"
 import { fileURLToPath } from "url"
 import { dirname, join } from "path"
-import fs from "fs"
 import uniqid from "uniqid"
 import createHttpError from "http-errors"
-import authorsRouter from "./services/authors"
+import { readContent, writeContent } from "../../functions/readAndWrite.js"
+import { blogPostValidation } from "./validation"
 
 const blogPostsJSONPath = join(
   dirname(fileURLToPath(import.meta.url)),
@@ -13,10 +13,59 @@ const blogPostsJSONPath = join(
 
 const blogPostsRouter = express.Router() // provide Routing
 
-authorsRouter.get("/", (req, res, next) => {})
-authorsRouter.get("/:id", (req, res, next) => {})
-authorsRouter.post("/", (req, res, next) => {})
-authorsRouter.put("/:id", (req, res, next) => {})
-authorsRouter.delete("/:id", (req, res, next) => {})
+blogPostsRouter.get("/", (req, res, next) => {
+  try {
+    const posts = readContent(blogPostsJSONPath)
+
+    if (req.query && req.query.category) {
+      const categoryPosts = posts.filter((post) =>
+        post.category.includes(req.query.category)
+      )
+      res.send(categoryPosts)
+    } else {
+      res.send(posts)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogPostsRouter.get("/:_id", (req, res, next) => {
+  try {
+    const posts = readContent(blogPostsJSONPath)
+    const post = posts.find((p) => p._id === req.params._id)
+    if (post) {
+      res.send(post)
+    } else {
+      res.send(createHttpError(404, `404 post with the id:${req.params._id}`))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogPostsRouter.post("/", (req, res, next) => {
+  try {
+    res.status(201).send("here it is")
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogPostsRouter.put("/:_id", (req, res, next) => {
+  try {
+    res.send("here it is")
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogPostsRouter.delete("/:_id", (req, res, next) => {
+  try {
+    res.status(204).send("here it is")
+  } catch (error) {
+    next(error)
+  }
+})
 
 export default blogPostsRouter // export Routing

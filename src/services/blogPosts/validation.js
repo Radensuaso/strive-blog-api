cc09aed4bb0699d7de5aa851bbcea63f8ee15009
Radensuaso <andrels9283@gmail.com>
2021-08-11 16:27:25 +0100

    Corrected bug in validation

--- src/services/blogPosts/validation.js
+++ src/services/blogPosts/validation.js
@@ -3,15 +3,16 @@ import { body } from "express-validator"
 export const blogPostValidation = [
   body("category").exists().withMessage("category is a mandatory field!"),
   body("title").exists().withMessage("title is a mandatory field!"),
-  body("cover").exists().withMessage("cover is a mandatory field!") /* 
-  body("readTime".value)
+  body("cover").exists().withMessage("cover is a mandatory field!"),
+  body("readTime.value")
     .exists()
     .isNumeric()
     .withMessage("readTime value is a mandatory field!"),
-  body("readTime".unit)
+  body("readTime.unit")
     .exists()
     .withMessage("readTime unit is a mandatory field!"),
-  body("author".name).exists().withMessage("name is a mandatory field!"),
-  body("author".avatar).exists().withMessage("avatar is a mandatory field!"), */,
+  body("author.name").exists().withMessage("name is a mandatory field!"),
+  body("author.avatar").exists().withMessage("avatar is a mandatory field!"),
+
   body("content").exists().withMessage("content is a mandatory field!"),
 ]
